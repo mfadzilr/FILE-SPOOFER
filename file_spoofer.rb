413c71b9d3d3dbb1bf6e7877a058a943f512e48e
mfadzilr <mind1355@gmail.com>
2023-12-24 16:59:23 +0800

    change spoof to fake, checks for file exists

--- file_spoofer.rb
+++ file_spoofer.rb
@@ -16,12 +16,12 @@ OptionParser.new do |opts|
     options[:output] = output
   end
 
-  opts.on("-e", "--ext flash,pdf,xlsx,ppt,pptx-blue,pptx-green,pptx-orange,doc,docx", "file extension") do |ext|
+  opts.on("-e", "--ext flash, pdf, xlsx, ppt, pptx, doc, docx", "file extension") do |ext|
     options[:ext] = ext
   end
 
-  opts.on("-s", "--spoof", "spoof file extension") do
-    options[:spoof] = true
+  opts.on("-f", "--fake", "fake file extension") do
+    options[:fake] = true
   end
 
 end.parse!
@@ -32,29 +32,34 @@ p ARGV
 case options[:ext]
 when 'pdf'
   options[:icon] = "acrobat.ico"
-  options[:spoof_ext] = "pdf"
+  options[:fake_ext] = "pdf"
 when 'xlsx'
   options[:icon] = "Microsoft-Excel.ico"
-  options[:spoof_ext] = "xlsx"
+  options[:fake_ext] = "xlsx"
 when 'doc'
   options[:icon] = "Microsost-Word-2013.ico"
-  options[:spoof_ext] = "docx"
+  options[:fake_ext] = "doc"
 when 'docx'
   options[:icon] = "Microsoft-Word-2016.ico"
-  options[:spoof_ext] = "docx"
+  options[:fake_ext] = "docx"
 when 'ppt'
   options[:icon] = "powerpoint.ico"
-  options[:spoof_ext] = "ppt"
+  options[:fake_ext] = "ppt"
 when 'pptx'
   options[:icon] = "PowerPoint-orange.ico"
-  options[:spoof_ext] = "pptx"
+  options[:fake_ext] = "pptx"
 else
   puts "[!] Extension not supported"
+  exit
 end
 
-if options[:spoof]
-  filename, extension = options[:output].split('.') 
-  options[:output] = filename + "\u202E" + options[:spoof_ext].reverse! + "." + extension
+if File.exists?(options[:input])
+  filename, extension = options[:output].split('.')
+  if options[:fake]
+    options[:output] = filename + "\u202E" + options[:fake_ext].reverse! + "." + extension
+  else
+    options[:output] = filename + "." + options[:fake_ext] + "." + extension
+  end
 end
 
 if options[:output] && options[:input] && options[:icon]
